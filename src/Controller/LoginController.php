3ad7e4fa7d3e5f7902e2fde19e901b2f8c91e69e
Anastasiia <an.winx06@yahoo.com>
2024-08-01 19:03:39 +0300

    CVM-2: Basic auth

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,12 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/', name: 'home', methods: ['GET'])]
-    public function index(): Response
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        return new Response('Home page');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(): never
+    {
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
